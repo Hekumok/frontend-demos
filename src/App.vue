d50c26aa5e85db8a31e59187c8b095d6faae4040
Artemov Nikita <neki712@yandex.ru>
2018-10-14 18:21:46 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <country-suggest />
+    </div>
+</template>
+
+<script>
+import CountrySuggest from '@/tasks/4';
+
+export default {
+  name: 'app',
+  components: {
+    CountrySuggest,
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/tasks/4/CountrySuggest.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="country-suggest">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CountrySuggest',
+};
+</script>
+
+<style>
+
+</style>
